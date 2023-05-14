9aeb6208baabacec4962d3a025d2ea1685b808b9
soundwanders <soundwanders@gmail.com>
2023-05-14 13:39:17 -0400

    Type select changed to checkboxes. Form control icon colors cleaned up for light/dark theme contrast

--- components/ui/FormUI.jsx
+++ components/ui/FormUI.jsx
@@ -26,65 +26,103 @@ export const FormUI = (props) => {
             Recipe Types
           </label>
 
-          <select
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-200 
-            leading-tight focus:outline-none focus:shadow-outline"
-            id="types"
-            name="types"
-            multiple
-            value={props.recipe.types || []}
-            onChange={props.handleChange}
-          >
-            <option value="">Select Type</option>
-            <option 
-              value="beef" 
-              className={props.recipe.types?.includes("beef") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Beef
-            </option>
-            <option 
-              value="chicken" 
-              className={props.recipe.types?.includes("chicken") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Chicken
-            </option>
-            <option 
-              value="pork" 
-              className={props.recipe.types?.includes("pork") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Pork
-            </option>
-            <option 
-              value="salad" 
-              className={props.recipe.types?.includes("salad") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Salad
-            </option>
-            <option 
-              value="seafood" 
-              className={props.recipe.types?.includes("seafood") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Seafood
-            </option>
-            <option 
-              value="soup" 
-              className={props.recipe.types?.includes("soup") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Soup
-            </option>
-            <option 
-              value="texmex" 
-              className={props.recipe.types?.includes("texmex") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              TexMex
-            </option>
-            <option 
-              value="vegan" 
-              className={props.recipe.types?.includes("vegan") ? "selected focus:bg-orange-100 dark:focus:bg-slate-800 bg-orange-100 dark:bg-slate-800" : ""}
-            >
-              Vegan
-            </option>
-          </select>
+          <div className="flex flex-wrap gap-4">
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="beef"
+                checked={props.recipe.types?.includes("beef")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-500 border border-gray-400 rounded shadow-sm"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Beef</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="chicken"
+                checked={props.recipe.types?.includes("chicken")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Chicken</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="pork"
+                checked={props.recipe.types?.includes("pork")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Pork</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="salad"
+                checked={props.recipe.types?.includes("salad")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Salad</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="seafood"
+                checked={props.recipe.types?.includes("seafood")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Seafood</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="soup"
+                checked={props.recipe.types?.includes("soup")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Soup</span>
+            </label>
+
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="texmex"
+                checked={props.recipe.types?.includes("texmex")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">TexMex</span>
+            </label>
+                    
+            <label className="inline-flex items-center">
+              <input
+                type="checkbox"
+                name="types"
+                value="vegan"
+                checked={props.recipe.types?.includes("vegan")}
+                onChange={props.handleChange}
+                className="form-checkbox h-5 w-5 text-orange-600 checked:bg-orange-350"
+              />
+              <span className="ml-2 text-gray-700 dark:text-gray-200">Vegan</span>
+            </label>
+          </div>
         </div>
 
         <div className="mb-8">
@@ -265,13 +303,13 @@ export const FormUI = (props) => {
                           <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                             <div className="flex mb-2">
                               <div className="w-10">
-                                <span className="inline-flex items-center justify-center w-4 h-4 p-2 mt-9 ml-2 rounded-full 
+                                <span className="inline-flex items-center justify-center w-4 h-4 p-2 mt-9 mx-1 rounded-full 
                                   border border-slate-700 dark:border-slate-100 bg-transparent text-slate-900 dark:text-white text-[10px]"
                                 >
                                   {index + 1}
                                 </span>
                               </div>
-                              <div className="flex-1 ml-2 pt-6">
+                              <div className="flex-1 pt-6">
                                 <input
                                   className="shadow appearance-none rounded-lg dark:border dark:border-slate-100 w-full h-auto py-2 px-4 text-gray-700 dark:text-gray-200 
                                   leading-tight focus:outline-none focus:shadow-outline focus:placeholder-transparent"
@@ -284,10 +322,10 @@ export const FormUI = (props) => {
                                   {...provided.dragHandleProps}
                                 />
                               </div>
-                              <div className="w-10 ml-2 flex justify-end p-2">
+                              <div className="w-10 mx-2 flex justify-end p-2">
                                 <button
                                   type="button"
-                                  className="inline-flex items-center justify-center mt-6 mr-2 rounded-full
+                                  className="inline-flex items-center justify-center mt-6 mr-1 rounded-full
                                   hover:text-red-500 focus:outline-none focus:bg-transparent focus:translate-y-[1px]"
                                   onClick={() => props.handleRemoveDirection(index)}
                                 >
@@ -309,7 +347,7 @@ export const FormUI = (props) => {
                 <button
                   type="button"
                   className="inline-flex items-center justify-center py-2 mr-4 
-                  hover:text-green-400 focus:outline-none focus:bg-transparent focus:translate-y-[1px]"
+                  hover:text-green-500 dark:hover:text-green-300 focus:outline-none focus:bg-transparent focus:translate-y-[1px]"
                   onClick={props.handleAddDirection}
                 >
                   <span className="sr-only">New Line</span>
@@ -319,7 +357,7 @@ export const FormUI = (props) => {
                 <button
                   type="button"
                   className="inline-flex items-center justify-center py-2 px-4 
-                  hover:text-red-400 dark:hover:text-red-300 focus:outline-none focus:bg-transparent focus:translate-y-[1px]"
+                  hover:text-orange-500 dark:hover:text-yellow-200 focus:outline-none focus:bg-transparent focus:translate-y-[1px]"
                   onClick={props.handleClearDirections}
                 >
                   <span className="sr-only">Clear Directions</span>
